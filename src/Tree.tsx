5b0cfbb4d2d400ca21fc8e7ed8bbe0c6f9f7ed26
AkhilchandranR <akhilchandran771@gmail.com>
2023-02-05 21:54:51 +0530

    tkt5 solved

--- src/Tree.tsx
+++ src/Tree.tsx
@@ -1,15 +1,27 @@
 import React,{useState,useCallback} from 'react';
 import { treeNode } from './data';
+import axios from 'axios';
 
 
 
 function Tree({id,name,children}:treeNode) {
 
     const[show,setShow] = useState(false);
+    const[data,setData] = useState([]);
 
     const handleClick = useCallback(
-      () => {
+     async () => {
         setShow(!show)
+
+        try{
+          const d = await axios.get('https:/dummyjson.com/carts')
+          console.log(d?.data?.carts);
+          setData(d?.data?.carts?.products)
+
+        }catch(e){
+          console.log(e)
+        }
+
       },
       [show,setShow],
     )
